8e545b9a0d8df09b294c55731a988e94db6c7dc9
Emanoel Eike Ferreira da Silva <eike.em.2017@gmail.com>
2021-08-12 04:01:25 -0300

    Primeiro commit

---
+++ js/owl/setup.js
@@ -0,0 +1,39 @@
+$('.carousel-1').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+//Capa
+var owl = $('.carousel-2');
+owl.owlCarousel({
+    items:1,
+    loop:true,
+    margin:5,
+    autoplay:true,
+    autoplayTimeout:4000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+});
+
